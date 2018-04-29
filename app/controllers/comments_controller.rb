6f3a53a4ffc8ca0f3e8e56677e4e3621bb477bcd
kailagoff <kaila.goff@gmail.com>
2018-04-29 13:13:53 -0700

    add comments controller and form, edit post views

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.comment_author = current_user.username
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to post_url(@post) }
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to posts_path(@post)
+  end
+
+private
+  def comment_params
+    params.require(:comment).permit(:author, :comment_content)
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  belongs_to :post
   validates :comment_content, :presence => true
   validates :author, :presence => true
   validates :post_id, :presence => true
