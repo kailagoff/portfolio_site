5a7d41245c29ccbaf83169a2d1bfb8293582d064
kailagoff <kaila.goff@gmail.com>
2018-04-29 10:39:49 -0700

    add bootstrap to forms and take away duplicate field names

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,6 @@
 class Skill < ActiveRecord::Base
   validates :skill_name, :presence => true
   validates :skill_description, :presence => true
-  has_attached_file :image, styles: { thumb: "150x150#" }
+  has_attached_file :image, styles: { thumb: "125x125#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
