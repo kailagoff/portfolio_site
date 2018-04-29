b5aeee54d4a7f64a5dae5e6d5c4d62e537788c28
kailagoff <kaila.goff@gmail.com>
2018-04-28 21:23:42 -0700

    begin populating database and styling of skills page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   validates :project_name, :presence => true
   validates :project_link, :presence => true
-  has_attached_file :image, styles: { large: "250x250#" }
+  has_attached_file :image, styles: { thumb: "250x250#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
