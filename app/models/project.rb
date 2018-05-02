71ed4acaa78a163eb0076203cc8ded2c6b07b79e
kailagoff <kaila.goff@gmail.com>
2018-05-02 12:55:20 -0700

    adjust and style projects pages

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   validates :project_name, :presence => true
   validates :project_link, :presence => true
-  has_attached_file :image, styles: { thumb: "250x250#" }
+  has_attached_file :image, styles: { large: "400x200#", thumb: "250x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
