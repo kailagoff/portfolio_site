a76300f0bde59d622fdaa0f6802dec1dea6f560e
kailagoff <kaila.goff@gmail.com>
2018-05-08 12:58:38 -0700

    alter display of photos, add larger display of photo on show.html

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   validates :photo_name, :presence => true
   validates :photo_description, :presence => true
-  has_attached_file :image, styles: { large: "250x250#" }
+  has_attached_file :image, styles: { large: "450x450#", medium: "250x250#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
