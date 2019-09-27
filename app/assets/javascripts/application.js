eae87b428523aa5187ef9148d642e75118ff4901
kaila goff <kaila.goff@gmail.com>
2019-09-27 15:04:27 -0700

    attempt to get js working on heroku deployment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
 
 $(document).ready(function(e) {
   // Initializing modal.
